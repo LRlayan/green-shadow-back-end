8cea2ff979de18d8bcbb52eef681583f58480dc4
LRLayan <rameshlayan4@gmail.com>
2024-11-10 03:57:21 +0530

    contactNoValidator()

--- src/main/java/com/example/demo/util/Regex.java
+++ src/main/java/com/example/demo/util/Regex.java
@@ -11,4 +11,12 @@ public class Regex {
     public static Matcher emailValidator(String email){
         return Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$").matcher(email);
     }
+
+    public static Matcher roleValidator(String role){
+        return Pattern.compile("^[A-Z]+$").matcher(role);
+    }
+
+    public static Matcher contactNoValidator(String contact){
+        return Pattern.compile("^(?:0?(77|76|78|34|75|72|74)[0-9]{7}|(77|76|78|34|75|72|74)[0-9]{8})$").matcher(contact);
+    }
 }
