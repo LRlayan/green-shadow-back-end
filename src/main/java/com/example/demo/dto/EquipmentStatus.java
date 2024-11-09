dcfb6818720050b9e1d9f5c680468eb9e8428f63
LRLayan <rameshlayan4@gmail.com>
2024-11-10 02:29:02 +0530

    define status interface

---
+++ src/main/java/com/example/demo/dto/EquipmentStatus.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+import com.example.demo.dto.SuperDTO;
+
+import java.io.Serializable;
+
+public interface EquipmentStatus extends SuperDTO, Serializable {
+}
---
+++ src/main/java/com/example/demo/dto/VehicleStatus.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+import org.modelmapper.internal.bytebuddy.implementation.bind.annotation.Super;
+
+import java.io.Serializable;
+
+public interface VehicleStatus extends SuperDTO , Serializable {
+}
