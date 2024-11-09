7a84029afdaa7c22288356d52e8d71347475db52
LRLayan <rameshlayan4@gmail.com>
2024-11-10 02:47:30 +0530

    generateCropCode()

--- src/main/java/com/example/demo/util/AppUtil.java
+++ src/main/java/com/example/demo/util/AppUtil.java
@@ -1,8 +1,14 @@
 package com.example.demo.util;
 
 import java.util.Base64;
+import java.util.UUID;
 
 public class AppUtil {
+
+    public static String generateCropCode(){
+        return "CROP-" + (int)(Math.random() * 1000) + 1;
+    }
+
     public static String imageBase64(byte[] image){
         return Base64.getEncoder().encodeToString(image);
     }
