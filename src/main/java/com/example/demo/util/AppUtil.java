783a306de41ef77f0b285143bed8b453aded69f5
LRLayan <rameshlayan4@gmail.com>
2024-11-10 02:48:25 +0530

    generateEquipmentCode()

--- src/main/java/com/example/demo/util/AppUtil.java
+++ src/main/java/com/example/demo/util/AppUtil.java
@@ -9,6 +9,10 @@ public class AppUtil {
         return "CROP-" + (int)(Math.random() * 1000) + 1;
     }
 
+    public static String generateEquipmentCode(){
+        return "EQU-" + (int)(Math.random() * 1000) + 1;
+    }
+
     public static String imageBase64(byte[] image){
         return Base64.getEncoder().encodeToString(image);
     }
