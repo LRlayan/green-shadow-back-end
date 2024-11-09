1e12722661764a9e5ce8fcd0c9c0d9490a9cbab3
LRLayan <rameshlayan4@gmail.com>
2024-11-10 04:15:16 +0530

    modelMapper()

--- src/main/java/com/example/demo/InitializeApplication.java
+++ src/main/java/com/example/demo/InitializeApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class InitializeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InitializeApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 }
