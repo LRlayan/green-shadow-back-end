709c31c9ffa9122df170541075bef68adc86d029
LRLayan <rameshlayan4@gmail.com>
2024-11-09 14:00:19 +0530

    vehicle entity

---
+++ src/main/java/com/example/demo/entity/impl/VehicleEntity.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity.impl;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "vehicle")
+public class VehicleEntity {
+    @Id
+    private String vehicleCode;
+    private String licensePlateNumber;
+    private String Name;
+    private String category;
+    private String fuelType;
+    private String remark;
+}
