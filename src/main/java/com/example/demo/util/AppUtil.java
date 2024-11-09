9d43a877200d8f1d44c2cd19f3ba7126f5906471
LRLayan <rameshlayan4@gmail.com>
2024-11-10 02:53:40 +0530

    generateStaffEquDetailsCode()

--- src/main/java/com/example/demo/util/AppUtil.java
+++ src/main/java/com/example/demo/util/AppUtil.java
@@ -21,6 +21,10 @@ public class AppUtil {
         return "LOG-" + (int)(Math.random() * 1000) + 1;
     }
 
+    public static String generateStaffEquDetailsCode(){
+        return "STEQU-" + Math.random() + 1;
+    }
+
     public static String imageBase64(byte[] image){
         return Base64.getEncoder().encodeToString(image);
     }
