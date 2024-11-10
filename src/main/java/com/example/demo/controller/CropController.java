0afe4652ab67d2667a03034c5ee644b039d3bf87
LRLayan <rameshlayan4@gmail.com>
2024-11-10 16:47:45 +0530

    saveCrop()

--- src/main/java/com/example/demo/controller/CropController.java
+++ src/main/java/com/example/demo/controller/CropController.java
@@ -2,19 +2,54 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.CropStatus;
 import com.example.demo.dto.impl.CropDTO;
+import com.example.demo.dto.impl.FieldDTO;
+import com.example.demo.exception.DataPersistException;
+import com.example.demo.service.CropService;
+import com.example.demo.util.AppUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/crops")
+@CrossOrigin
 public class CropController {
+    @Autowired
+    private CropService cropService;
+
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveCrop() {
-       return null;
+    public ResponseEntity<Void> saveCrop(
+            @RequestPart("cropName") String cropName,
+            @RequestPart("scientificName") String scientificName,
+            @RequestPart("category") String category,
+            @RequestPart("season") String season,
+            @RequestPart("cropImage") MultipartFile cropImage,
+            @RequestPart("fieldList") List<FieldDTO> fieldList
+    ) {
+        try{
+            var cropDTO = new CropDTO();
+            cropDTO.setCropCode(AppUtil.generateCode("CROP-"));
+            cropDTO.setCropName(cropName);
+            cropDTO.setScientificName(scientificName);
+            cropDTO.setCategory(category);
+            cropDTO.setSeason(season);
+            cropDTO.setCropImage(AppUtil.imageBase64(cropImage.getBytes()));
+            cropDTO.setFieldList(fieldList);
+            cropService.saveCrop(cropDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(value = "/{cropId}",produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/com/example/demo/service/impl/CropServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CropServiceImpl.java
@@ -1,8 +1,13 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.dao.CropDAO;
 import com.example.demo.dto.CropStatus;
 import com.example.demo.dto.impl.CropDTO;
+import com.example.demo.entity.impl.CropEntity;
+import com.example.demo.exception.DataPersistException;
 import com.example.demo.service.CropService;
+import com.example.demo.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +16,18 @@ import java.util.List;
 @Service
 @Transactional
 public class CropServiceImpl implements CropService {
+    @Autowired
+    private CropDAO cropDAO;
+    @Autowired
+    private Mapping mapping;
+
     @Override
     public void saveCrop(CropDTO cropDTO) {
+        CropEntity saveCrop = cropDAO.save(mapping.toCropEntity(cropDTO));
 
+        if (saveCrop == null){
+            throw new DataPersistException("Crop is not saved.");
+        }
     }
 
     @Override
