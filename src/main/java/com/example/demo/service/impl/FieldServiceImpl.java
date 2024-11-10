752a4671db3bdbc6345ae1c31868ccb767d73fa0
LRLayan <rameshlayan4@gmail.com>
2024-11-11 00:49:35 +0530

    saveField()

--- src/main/java/com/example/demo/service/impl/FieldServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FieldServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.dao.FieldDAO;
 import com.example.demo.dto.FieldStatus;
 import com.example.demo.dto.impl.CropDTO;
 import com.example.demo.dto.impl.FieldDTO;
+import com.example.demo.entity.impl.FieldEntity;
+import com.example.demo.exception.DataPersistException;
 import com.example.demo.service.FieldService;
+import com.example.demo.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +17,16 @@ import java.util.List;
 @Service
 @Transactional
 public class FieldServiceImpl implements FieldService {
+    @Autowired
+    private FieldDAO fieldDAO;
+    @Autowired
+    private Mapping mapping;
     @Override
     public void saveField(FieldDTO fieldDTO) {
-
+        FieldEntity saveField = fieldDAO.save(mapping.toFieldEntity(fieldDTO));
+        if (saveField == null){
+            throw new DataPersistException("Field is not saved.");
+        }
     }
 
     @Override
