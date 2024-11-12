57012164a1bd8e575c421e18c23f32e594e585bc
LRLayan <rameshlayan4@gmail.com>
2024-11-13 02:53:48 +0530

    throw exception

--- src/main/java/com/example/demo/service/FieldService.java
+++ src/main/java/com/example/demo/service/FieldService.java
@@ -4,11 +4,12 @@ import com.example.demo.dto.FieldStatus;
 import com.example.demo.dto.impl.CropDTO;
 import com.example.demo.dto.impl.FieldDTO;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface FieldService {
     void saveField(FieldDTO fieldDTO);
-    List<FieldDTO> getAllField();
+    List<FieldDTO> getAllField() throws IOException, ClassNotFoundException;
     void deleteField(String id);
     void updateField(String id,CropDTO fieldDTO);
     FieldStatus getSelectedField(String fieldId);
