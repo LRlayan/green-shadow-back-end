4d3b11c5b4aaf43ab72fb7e82c8685810068a410
LRLayan <rameshlayan4@gmail.com>
2024-11-12 22:59:18 +0530

    findLastRowNative()

--- src/main/java/com/example/demo/dao/FieldDAO.java
+++ src/main/java/com/example/demo/dao/FieldDAO.java
@@ -1,10 +1,12 @@
 package com.example.demo.dao;
 
-import com.example.demo.entity.impl.CropEntity;
 import com.example.demo.entity.impl.FieldEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FieldDAO extends JpaRepository<FieldEntity,String> {
+    @Query(value = "SELECT * FROM field WHERE field_code = (SELECT field_code FROM field ORDER BY CAST(SUBSTRING(field_code, 7) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
+    FieldEntity findLastRowNative();
 }
