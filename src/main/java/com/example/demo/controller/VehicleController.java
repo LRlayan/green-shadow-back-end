770e8a27b0c37244f6030eafd499685e2a4da191
LRLayan <rameshlayan4@gmail.com>
2024-11-13 21:17:51 +0530

    save vehicle

--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -2,6 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.VehicleStatus;
 import com.example.demo.dto.impl.VehicleDTO;
+import com.example.demo.exception.DataPersistException;
+import com.example.demo.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +14,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/vehicles")
+@CrossOrigin
 public class VehicleController {
+    @Autowired
+    private VehicleService vehicleService;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveVehicle(@RequestBody VehicleDTO vehicleDTO){
-        return null;
+        try{
+            vehicleService.saveVehicle(vehicleDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(value = "/{vehicleId}",produces = MediaType.APPLICATION_JSON_VALUE)
