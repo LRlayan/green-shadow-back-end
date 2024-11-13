2e9d9d0dbef9ff562f05195ee15f5a6fda627a68
LRLayan <rameshlayan4@gmail.com>
2024-11-13 21:18:04 +0530

    save vehicle

--- src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
@@ -1,8 +1,14 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.dao.VehicleDAO;
 import com.example.demo.dto.VehicleStatus;
 import com.example.demo.dto.impl.VehicleDTO;
+import com.example.demo.entity.impl.CropEntity;
+import com.example.demo.entity.impl.VehicleEntity;
+import com.example.demo.exception.DataPersistException;
 import com.example.demo.service.VehicleService;
+import com.example.demo.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +17,25 @@ import java.util.List;
 @Service
 @Transactional
 public class VehicleServiceImpl implements VehicleService {
+    @Autowired
+    private VehicleDAO vehicleDAO;
+    @Autowired
+    private Mapping mapping;
+
     @Override
     public void saveVehicle(VehicleDTO vehicleDTO) {
+        int number = 0;
+        VehicleEntity vehicle = vehicleDAO.findLastRowNative();
+        if (vehicle != null){
+            String[] parts = vehicle.getVehicleCode().split("-");
+            number = Integer.parseInt(parts[1]);
+        }
+        vehicleDTO.setVehicleCode("VEHICLE-" + ++number);
 
+        VehicleEntity vehicleEntity = vehicleDAO.save(mapping.toVehicleEntity(vehicleDTO));
+        if (vehicleEntity == null){
+            throw new DataPersistException("vehicle not saved");
+        }
     }
 
     @Override
