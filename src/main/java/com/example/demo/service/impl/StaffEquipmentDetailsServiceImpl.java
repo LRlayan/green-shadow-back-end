3f1720da25d6b1fd5fe32c5acf88601f10dad673
LRLayan <rameshlayan4@gmail.com>
2024-11-15 12:55:51 +0530

    saveStaffEquipmentDetails()

--- src/main/java/com/example/demo/service/impl/StaffEquipmentDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StaffEquipmentDetailsServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.dao.StaffEquipmentDetailsDAO;
 import com.example.demo.dto.StaffEquipmentDetailsStatus;
 import com.example.demo.dto.impl.StaffEquipmentDetailsDTO;
+import com.example.demo.entity.impl.StaffEquipmentDetailsEntity;
 import com.example.demo.service.StaffEquipmentDetailsService;
+import com.example.demo.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +15,25 @@ import java.util.List;
 @Service
 @Transactional
 public class StaffEquipmentDetailsServiceImpl implements StaffEquipmentDetailsService {
+    @Autowired
+    private StaffEquipmentDetailsDAO staffEquipmentDetailsDAO;
+    @Autowired
+    private Mapping mapping;
+
     @Override
     public void saveStaffEquipmentDetails(StaffEquipmentDetailsDTO equipmentDetailsDTO) {
-
+        int number = 0;
+        StaffEquipmentDetailsEntity staffEquipmentDetails = staffEquipmentDetailsDAO.findLastRowNative();
+        if (staffEquipmentDetails != null){
+            String[] parts = staffEquipmentDetails.getId().split("-");
+            number = Integer.parseInt(parts[1]);
+        }
+        equipmentDetailsDTO.setId("STEQDET-" + ++number);
+        if (equipmentDetailsDTO == null){
+            System.out.println("null dto ==========================");
+        }
+        System.out.println(equipmentDetailsDTO);
+        staffEquipmentDetailsDAO.save(mapping.toStaffEquDetailsEntity(equipmentDetailsDTO));
     }
 
     @Override
