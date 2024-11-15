58a4892ebe1c9dca7968f0d6306dd7d9a0e1a298
LRLayan <rameshlayan4@gmail.com>
2024-11-15 12:28:58 +0530

    saveStaff()

--- src/main/java/com/example/demo/controller/StaffController.java
+++ src/main/java/com/example/demo/controller/StaffController.java
@@ -2,6 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.StaffStatus;
 import com.example.demo.dto.impl.StaffDTO;
+import com.example.demo.exception.DataPersistException;
+import com.example.demo.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +14,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/staff")
+@CrossOrigin
 public class StaffController {
+    @Autowired
+    private StaffService staffService;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO){
-        return null;
+        try{
+            staffService.saveStaffMember(staffDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(value = "/{staffId}",produces = MediaType.APPLICATION_JSON_VALUE)
