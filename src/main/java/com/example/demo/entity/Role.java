17f4ff87d5f50fcc4dc4914ca818036a731cc76d
LRLayan <rameshlayan4@gmail.com>
2024-11-15 12:26:58 +0530

    fromString()

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,5 +1,12 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Role {
-    MANAGER,ADMINISTRATIVE,SCIENTIST,OTHER
+    MANAGER,ADMINISTRATIVE,SCIENTIST,OTHER;
+
+    @JsonCreator
+    public static Role fromString(String value) {
+        return Role.valueOf(value.toUpperCase());
+    }
 }
