1eecb6660802d25f63b711aecbb874b5bd24c753
LRLayan <rameshlayan4@gmail.com>
2024-11-15 12:26:03 +0530

    saveEquipment()

--- src/main/java/com/example/demo/service/impl/EquipmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EquipmentServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.dao.EquipmentDAO;
 import com.example.demo.dto.EquipmentStatus;
 import com.example.demo.dto.impl.CropDTO;
 import com.example.demo.dto.impl.EquipmentDTO;
+import com.example.demo.entity.impl.EquipmentEntity;
+import com.example.demo.exception.DataPersistException;
 import com.example.demo.service.EquipmentService;
+import com.example.demo.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +17,24 @@ import java.util.List;
 @Service
 @Transactional
 public class EquipmentServiceImpl implements EquipmentService {
+    @Autowired
+    private EquipmentDAO equipmentDAO;
+    @Autowired
+    private Mapping mapping;
+
     @Override
     public void saveEquipment(EquipmentDTO equipmentDTO) {
-
+        int number = 0;
+        EquipmentEntity equipment = equipmentDAO.findLastRowNative();
+        if (equipment != null){
+            String[] parts = equipment.getEquipmentCode().split("-");
+            number = Integer.parseInt(parts[1]);
+        }
+        equipmentDTO.setEquipmentCode("EQUIPMENT-" + ++number);
+        EquipmentEntity equipmentEntity = equipmentDAO.save(mapping.toEquipmentEntity(equipmentDTO));
+        if (equipmentEntity == null){
+            throw new DataPersistException("Equipment not saved!");
+        }
     }
 
     @Override
