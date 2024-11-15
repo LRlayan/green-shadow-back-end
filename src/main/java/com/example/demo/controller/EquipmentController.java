1c1af791b83dd313580c8745effa663b21072dc9
LRLayan <rameshlayan4@gmail.com>
2024-11-15 12:25:44 +0530

    saveEquipment()

--- src/main/java/com/example/demo/controller/EquipmentController.java
+++ src/main/java/com/example/demo/controller/EquipmentController.java
@@ -2,6 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.EquipmentStatus;
 import com.example.demo.dto.impl.EquipmentDTO;
+import com.example.demo.exception.DataPersistException;
+import com.example.demo.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +14,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/equipment")
+@CrossOrigin
 public class EquipmentController {
+    @Autowired
+    private EquipmentService equipmentService;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDTO equipmentDTO){
-       return null;
+        try{
+            equipmentService.saveEquipment(equipmentDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(value = "/{equipmentId}",produces = MediaType.APPLICATION_JSON_VALUE)
