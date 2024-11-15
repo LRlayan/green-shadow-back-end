bc0330123e02e39a6b4495058e1893f5f63b6d76
LRLayan <rameshlayan4@gmail.com>
2024-11-15 12:30:23 +0530

    findLastRowNative()

--- src/main/java/com/example/demo/dao/StaffEquipmentDetailsDAO.java
+++ src/main/java/com/example/demo/dao/StaffEquipmentDetailsDAO.java
@@ -1,10 +1,13 @@
 package com.example.demo.dao;
 
-import com.example.demo.entity.impl.CropEntity;
 import com.example.demo.entity.impl.StaffEquipmentDetailsEntity;
+import com.example.demo.entity.impl.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StaffEquipmentDetailsDAO extends JpaRepository<StaffEquipmentDetailsEntity,String> {
+    @Query(value = "SELECT * FROM staff_equipment_details WHERE id = (SELECT id FROM staff_equipment_details ORDER BY CAST(SUBSTRING(id, 8) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
+    StaffEquipmentDetailsEntity findLastRowNative();
 }
